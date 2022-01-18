a980cbeea8cb015e35cd7a82153ccb7e6e14b7f3
Savira Saetang <www-best-123@hotmail.com>
2022-01-18 14:43:25 +0700

    Initial Commit

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Tim");
+        String name = "TimVar";
+        System.out.println(name);
+        int myFirstNum = 5;
+        int mySecondNum = 10;
+        int myThirdNum =20;
+        int myTotal = myFirstNum+mySecondNum+myThirdNum;
+        System.out.println(myTotal);
+    }
+}
